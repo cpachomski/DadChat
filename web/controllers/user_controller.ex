d18cb2b455b1d5f8e4ade0803338c7050f122757
Chris Pachomski <cpachomski@gmail.com>
2016-12-28 02:04:07 -0500

    add index action/template and create action for users

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -6,4 +6,18 @@ defmodule Dadchat.UserController do
 		changeset = User.changeset(%User{})
 		render conn, "new.html", changeset: changeset
 	end
+
+	def create(conn, %{"user" => user_params}) do
+		changeset = User.changeset(%User{}, user_params)
+		{:ok, user} = Repo.insert(changeset)
+
+		conn
+		|> put_flash(:info, "#{user.username} create!")
+		|> redirect(to: user_path(conn, :index))
+	end
+
+	def index(conn, _params) do
+		users = Repo.all(User)
+		render conn, "index.html", users: users
+	end
 end
