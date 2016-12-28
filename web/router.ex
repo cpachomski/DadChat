ee2b76674e562ebbf6676ee335fed24396d1990a
Chris Pachomski <cpachomski@gmail.com>
2016-12-28 02:36:38 -0500

    add auth to router

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Dadchat.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Dadchat.Auth, repo: Dadchat.Repo
   end
 
   scope "/", Dadchat do
