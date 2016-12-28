d22dfc6059bab7de92e4cf73c1159747f4b4ff45
Chris Pachomski <cpachomski@gmail.com>
2016-12-28 01:41:18 -0500

    add user model, changeset, and migration

---
+++ web/models/user.ex
@@ -0,0 +1,22 @@
+defmodule Dadchat.User do
+	use Dadchat.Web, :model
+
+	schema "users" do
+		field :first_name, :string
+		field :last_name, :string
+		field :username, :string
+		field :email, :string
+		field :password, :string, virtual: true
+		field :password_hash, :string
+
+		timestamps
+	end
+
+	def changeset(model, params \\ :invalid) do
+		model 
+		|> cast(params, ~w(first_name last_name username email))
+		|> validate_length(:username, min: 1, max: 20)
+		|> unique_constraint(:username)
+		|> unique_constraint(:username)
+	end
+end
