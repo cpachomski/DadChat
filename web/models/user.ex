6df766f9f162d1e610e64f33ed8a32d26f47e8d9
Chris Pachomski <cpachomski@gmail.com>
2016-12-28 02:26:22 -0500

    create registration changeset and change user pw into hashed pw upon creation

--- web/models/user.ex
+++ web/models/user.ex
@@ -15,23 +15,24 @@ defmodule Dadchat.User do
 		|> cast(params, ~w( username email ))
 		|> validate_length(:username, min: 3, max: 20)
 		|> unique_constraint(:username)
-		|> unique_constraint(:username)
+		|> unique_constraint(:email)
+		|> validate_format(:email, ~r/(\w+)@([\w.]+)/)
 	end
 
-	# def registration_changeset(model, params) do
-	# 	model
-	# 	|> changeset(params)
-	# 	|> cast(params, ~w(password), [])
-	# 	|> validate_length(:password, min: 6, max: 100)
-	# 	|> put_pass_hash()
-	# end
+	def registration_changeset(model, params) do
+		model
+		|> changeset(params)
+		|> cast(params, ~w(password), [])
+		|> validate_length(:password, min: 6, max: 100)
+		|> put_pass_hash()
+	end
 
-	# def put_pass_hash(changeset) do
-	# 	case changeset do
-	# 		%Ecto.Changeset{valid?: true, changes: %{ password: p}} ->
-	# 			put_change(changeset, :password_hash, Comeonin,Bcrypt.hasspwsalt(p))
-	# 		_ ->
-	# 			changeset
-	# 	end
-	# end
+	defp put_pass_hash(changeset) do
+		case changeset do
+			%Ecto.Changeset{valid?: true, changes: %{ password: p}} ->
+				put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(p))
+			_ ->
+				changeset
+		end
+	end
 end
