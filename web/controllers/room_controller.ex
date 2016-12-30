c5c0499a65a6b301d13487275d14a275d4d49f8d
Chris Pachomski <cpachomski@gmail.com>
2016-12-30 15:17:24 -0500

    add  current user to room action arguments

--- web/controllers/room_controller.ex
+++ web/controllers/room_controller.ex
@@ -5,15 +5,19 @@ defmodule Dadchat.RoomController do
 
   alias Dadchat.Room
 
-  def index(conn, _params) do
+  def action(conn, _) do
+    apply(__MODULE__, action_name(conn),
+      [ conn, conn.params, conn.assigns.current_user ])
+  end
+
+  def index(conn, _params, user) do
     rooms = Repo.all(Room)
     render(conn, "index.html", rooms: rooms)
   end
 
-  def new(conn, _params) do
+  def new(conn, _params, user) do
     changeset = Room.changeset(%Room{})
     render(conn, "new.html", changeset: changeset)
   end
-
 
 end
